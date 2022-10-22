a90df19290c97df1e5037e750175b9e63b75d50b
konchellah <williamoriginal2019@gmail.com>
2022-10-22 10:53:22 +0300

    Second

--- db.sql
+++ db.sql
@@ -1,10 +1,19 @@
+DROP TABLE Report;
+
 CREATE TABLE IF NOT EXISTS Report(
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
   latitude VARCHAR(20) NOT NULL,
   longitude VARCHAR(20) NOT NULL,
-  time DATE NOT NULL,
+  time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   who VARCHAR(20),
   description TEXT,
   gender VARCHAR(10),
   name VARCHAR(30)
 )
+
+
+INSERT INTO Report (latitude,longitude,who,description,gender,name)
+VALUES ("12.4","45.67","me","whaoejsbvbjvjksbv akbgjbaf","female","name1")
+
+INSERT INTO Report (latitude,longitude,who,description,gender,name)
+VALUES ("-1.3030868", "36.81425", "me","whaoejsbvbjvjksbv akbgjbaf","female","name1")
--- index.js
+++ index.js
@@ -2,54 +2,55 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'dbuser',
-//     password: 's3kreee7',
-//     database: 'my_db'
-// })
+const connection = mysql.createConnection({
+    host: 'database-2.cmwdr0ca6gea.us-east-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'Impacters100%',
+    database: 'reports'
+})
 
-// connection.connect()
+
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+  });
+
 
 const app = express();
 app.use(bodyParser.json())
 
 
-app.post('/report', (req,res)=>{
-   
-    const {
+
+app.post('/report', async (req,res)=>{
+    let data = {
         latitude,
         longitude,
         name,
         who,
         description,
-        gender,
-        time
+        gender
         
     } = req.body;
 
-    res.json({
-        latitude,
-        longitude,
-        name,
-        who,
-        description,
-        gender,
-        time
-        
-    })
+        try {
+            await connection.query('INSERT INTO Report SET ?', data, function (error, results, fields) {
+               
+                res.status(200).send(results.insertId);
+                return
+              });
+            
+        } catch (error) {
+            
+            res.status(400).send(error);
+            return;
+        }
 
-    console.log({
-        latitude,
-        longitude,
-        name,
-        who,
-        description,
-        gender,
-        time
-    })
+    res.sendStatus(200)
 
 })
 
 app.listen(3000,()=>console.log("Listening"));
-
